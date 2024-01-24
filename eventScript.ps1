4b6cb04b62f88cff45b59303e596f362b2b6138f
Tomás Gentile <102368599+tomasgentilee@users.noreply.github.com>
2024-01-24 11:30:51 -0300

    Add files via upload

---
+++ eventScript.ps1
@@ -0,0 +1,16 @@
+﻿$ScriptStartTime = (Get-Date).Date.AddDays(-1)
+$ScriptEndTime = (Get-Date).Date
+
+$LogOutputDirectory = 'C:\Users\Segadmin\Documents\EVTVW Export'
+
+$EventTypesToExport = @('Application', 'Security', 'Setup', 'System')
+
+foreach ($EventType in $EventTypesToExport)
+{
+        $LogOutputTopic = "$EventType Log"
+        $CurrentTimeUTC = Get-Date
+        $LogOutputFileName = "$CurrentTimeUTC - $LogOutputTopic"
+        $LogOutputCSVFilePath = "$LogOutputDirectory\$LogOutputFileName.csv"
+
+        Get-WinEvent -FilterHashtable @{logname="$EventType";StartTime="$ScriptStartTime"} |  Export-CSV -Path "$LogOutputCSVFilePath"
+}
---
+++ logsPrimerFiltro.ps1
@@ -0,0 +1,35 @@
+﻿[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$false)]
+    [string]$output="C:\Users\Segadmin\Documents\EVTVW",
+    [Parameter(Mandatory=$false)]
+    $excludeEvtxFiles = ((get-eventlog -list) | foreach-object{$_.log}),
+    [Parameter(Mandatory=$false)]
+    $logTag = $env:ComputerName,
+    [Parameter(Mandatory=$false)]
+    $CurrentDate = "-" + (Get-Date).ToString('yyyy-MM-dd HH:mm:ss')   
+)
+
+if ($excludeEvtxFiles) {
+    $excludeEvtxFiles | ForEach-Object {
+        $LogName = "$LogTag-" + $_
+        Try {
+            Get-EventLog $_ -ErrorAction Stop |
+                select @{name="containerLog";expression={$LogName}},
+                    @{name="id";expression={$_.EventID}},
+                    @{name="levelDisplayName";expression={$_.EntryType}},
+                    MachineName,
+                    @{name="LogName";expression={$LogName}},
+                    ProcessId,
+                    @{name="UserId";expression={$_.UserName}},
+                    @{name="ProviderName";expression={$_.source}},
+                    @{Name="TimeCreated";expression={(($_.TimeGenerated).ToUniversalTime()).ToString('yyyy-MM-dd HH:mm:ssZ')}},
+                    @{Name="Message";expression={$_.message -replace "\r\n"," | " -replace "\n", " | " -replace "The local computer may not have the necessary registry information or message DLL files to display the message, or you may not have permission to access them.",""}} | 
+               Export-Csv -NoTypeInformation ($output + "\" + "$LogTag-" + $_ + ".csv")
+        }
+        Catch {
+            Write-Verbose "Previous Log doesn't have any records. No output will be produced"
+        }
+        
+    }
+}
