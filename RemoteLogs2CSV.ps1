298924e935dcd101d832dd752fc3e9224f45be4f
Tomás Gentile <102368599+tomasgentilee@users.noreply.github.com>
2024-01-25 13:06:11 -0300

    Update RemoteLogs2CSV.ps1

--- RemoteLogs2CSV.ps1
+++ RemoteLogs2CSV.ps1
@@ -7,31 +7,36 @@ Param (
 # Obtener la fecha actual en el formato deseado
 $CurrentDate = Get-Date -Format 'yyyy-MM-dd'
 
-# Obtener eventos del log ForwardedEvents
-Try {
-    Get-WinEvent -LogName ForwardedEvents -ErrorAction Stop |
-        Where-Object { ($_.ProviderName -ne 'SynTPEnhService') -and ($_.TimeCreated -ge (Get-Date $CurrentDate)) } |
-        Group-Object -Property MachineName |
-        ForEach-Object {
-            $machineName = $_.Name
-            $MachineFolderPath = Join-Path -Path $output -ChildPath $machineName
-            New-Item -ItemType Directory -Force -Path $MachineFolderPath | Out-Null
+# Lista de logs a exportar
+$logsToExport = @("Security", "Application", "System")
 
-            $events = $_.Group
-            $events |
-            Select-Object @{Name="containerLog";Expression={'ForwardedEvents'}},
-                @{Name="id";Expression={$_.Id}},
-                @{Name="levelDisplayName";Expression={$_.LevelDisplayName}},
-                MachineName,
-                @{Name="LogName";Expression={'ForwardedEvents'}},
-                ProcessId,
-                @{Name="UserId";Expression={$_.Properties[8].Value}},
-                @{Name="ProviderName";Expression={$_.ProviderName}},
-                @{Name="TimeCreated";Expression={$_.TimeCreated.ToUniversalTime().ToString('yyyy-MM-dd HH:mm:ssZ')}},
-                @{Name="Message";Expression={$_.Message -replace "\r\n"," | " -replace "\n", " | " -replace "The local computer may not have the necessary registry information or message DLL files to display the message, or you may not have permission to access them.",""}} | 
-                Export-Csv -NoTypeInformation -Path (Join-Path -Path $MachineFolderPath -ChildPath "ForwardedEvents-$CurrentDate.csv")
-        }
-}
-Catch {
-    Write-Verbose "No se pudieron encontrar registros en el log ForwardedEvents en la fecha $CurrentDate."
+foreach ($logName in $logsToExport) {
+    Try {
+        Get-WinEvent -LogName ForwardedEvents -FilterXPath ("<QueryList><Query Id='0' Path='ForwardedEvents'><Select Path='ForwardedEvents'>*[System[TimeCreated[@SystemTime&gt;='{0:yyyy-MM-ddTHH:mm:ss}' and @SystemTime&lt;'{1:yyyy-MM-ddTHH:mm:ss}']]]</Select></Query></QueryList>" -f $startTime, $endTime) -ErrorAction Stop |
+            ## Filtra por el providerName
+            Where-Object { ($_.ProviderName -ne 'SynTPEnhService') } |
+            Group-Object -Property MachineName |
+            ForEach-Object {
+                $machineName = $_.Name
+                $MachineFolderPath = Join-Path -Path $output -ChildPath $machineName
+                New-Item -ItemType Directory -Force -Path $MachineFolderPath | Out-Null
+
+                $events = $_.Group
+                $events |
+                Select-Object @{Name="containerLog";Expression={$logName}},
+                    @{Name="id";Expression={$_.Id}},
+                    @{Name="levelDisplayName";Expression={$_.LevelDisplayName}},
+                    MachineName,
+                    @{Name="LogName";Expression={$logName}},
+                    ProcessId,
+                    @{Name="UserId";Expression={$_.Properties[8].Value}},
+                    @{Name="ProviderName";Expression={$_.ProviderName}},
+                    @{Name="TimeCreated";Expression={$_.TimeCreated.ToUniversalTime().ToString('yyyy-MM-dd HH:mm:ssZ')}},
+                    @{Name="Message";Expression={$_.Message -replace "\r\n"," | " -replace "\n", " | " -replace "The local computer may not have the necessary registry information or message DLL files to display the message, or you may not have permission to access them.",""}} | 
+                    Export-Csv -NoTypeInformation -Path (Join-Path -Path $MachineFolderPath -ChildPath "$logName-$CurrentDate.csv")
+            }
+    }
+    Catch {
+        Write-Verbose "No se pudieron encontrar registros en el log ForwardedEvents en la fecha $CurrentDate."
+    }
 }
